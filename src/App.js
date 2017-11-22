7c0f164dab023eac7999f6e190bba96e9dd89242
Simon Sturmer <sstur@me.com>
2017-11-22 16:22:04 +0700

    Start basic navigator with three scenes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 // @flow
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-export default class App extends Component<{}> {
+const HOME = 'home';
+const SEARCH = 'search';
+const BROWSE = 'browse';
+
+type Props = {};
+type State = {
+  currentPage: string,
+};
+
+export default class App extends Component<Props, State> {
+  state = {
+    currentPage: HOME,
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello BTPN</Text>
-      </View>
-    );
+    switch (this.state.currentPage) {
+      case HOME: {
+        return (
+          <View style={styles.container}>
+            <Text>Welcome</Text>
+            <Button
+              title="Search"
+              onPress={() => this.setState({currentPage: SEARCH})}
+            />
+            <Button
+              title="Browse"
+              onPress={() => this.setState({currentPage: BROWSE})}
+            />
+          </View>
+        );
+      }
+      case SEARCH: {
+        return (
+          <View style={styles.container}>
+            <Text>Search</Text>
+            <Button
+              title="Back"
+              onPress={() => this.setState({currentPage: HOME})}
+            />
+          </View>
+        );
+      }
+      case BROWSE: {
+        return (
+          <View style={styles.container}>
+            <Text>Browse</Text>
+            <Button
+              title="Back"
+              onPress={() => this.setState({currentPage: HOME})}
+            />
+          </View>
+        );
+      }
+      default: {
+        throw new Error('Unknown route.');
+      }
+    }
   }
 }
 
